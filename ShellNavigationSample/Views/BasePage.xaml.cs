8ce52e0e38b351a3d7f5566d17822509c0375cf6
Heinz Holzer <holzerheinz@hotmail.com>
2024-01-26 13:03:17 +0100

    add Param1 & Param2 to BasePage

--- ShellNavigationSample/Views/BasePage.xaml.cs
+++ ShellNavigationSample/Views/BasePage.xaml.cs
@@ -1,10 +1,26 @@
 using ShellNavigationSample.ViewModels.Base;
+using System.Diagnostics;
 
 namespace ShellNavigationSample.Views;
 
-public partial class BasePage : ContentPage
+[QueryProperty(nameof(Param1), "p1")]
+[QueryProperty(nameof(Param2), "p2")]
+public partial class BasePage : ContentPage, IQueryAttributable
 {
     private bool _isInitialized;
+    private string _param1;
+    public string Param1
+    {
+        get { return _param1; }
+        set { if (_param1 == value) return; _param1 = value; OnPropertyChanged(); }
+    }
+
+    private string _param2;
+    public string Param2
+    {
+        get { return _param2; }
+        set { if (_param2 == value) return; _param2 = value; OnPropertyChanged(); }
+    }
 
     public IList<IView> BasePageContent => BaseContentGrid.Children;
     public Label ShellPreviousLabel => ShellPrevious;
@@ -35,5 +51,13 @@ public partial class BasePage : ContentPage
         RouteEntry.Text = path;
         NaviPath.Text = path;
         Info.Text = $"last navigated to at {DateTime.Now:HH:mm:ss}";
+    }
+
+    public void ApplyQueryAttributes(IDictionary<string, object> query)
+    {
+        foreach (var kvp in query)
+        {
+            Debug.WriteLine($"{kvp.Key}={kvp.Value}");
+        }
     }
 }
